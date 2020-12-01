a1738099c01039ecf8987d484517896f61909666
Gidey <Gidey@Gidey.local>
2020-11-30 22:51:52 -0500

    complete first page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Categories from "./Categories";
 import Services from "./Services";
-import image from "../images/home-banner.png";
+import home_banner from "../images/home-banner.png";
 
 function Home() {
   return (
@@ -10,31 +10,39 @@ function Home() {
         <h1>Write for readers not for search engines.</h1>
       </header>
       <section>
-        <p>
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard.
-        </p>
+        <div>
+          <p>
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard.
+          </p>
+          <button>CHECK OUR PROJECTS</button>
+        </div>
+
+        <div className="img">
+          <img src={home_banner} alt="home bunner" />
+        </div>
       </section>
-      <div className="button">
-        <button>CHECK OUR PROJECTS</button>
-      </div>
-      <div className="img">
-        <img src={image} alt="home bunner" />
-      </div>
 
       <div className="categories-home">
         <Categories num="34" description="Happy Clients" />
       </div>
       <div>
-        <Services
-          service="Website Development"
-          description="We design professional looking yet simple websies. OUr designs are search engle and user friendly"
-        />
+        <h1>Service We Provide</h1>
+        <p>
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book
+        </p>
 
-        <Services
-          service="Website Development"
-          description="We design professional looking yet simple websies. OUr designs are search engle and user friendly"
-        />
+        <div className="service-items">
+          <Services
+            service="Website Development"
+            description="We design professional looking yet simple websies. OUr designs are search engle and user friendly"
+          />
+          <Services
+            service="Website Development"
+            description="We design professional looking yet simple websies. OUr designs are search engle and user friendly"
+          />
+        </div>
       </div>
     </div>
   );
--- src/Home/Services.jsx
+++ src/Home/Services.jsx
@@ -3,13 +3,7 @@ import React from "react";
 export default function Services(props) {
   return (
     <div className="services">
-      <h1>Service We Provide</h1>
-      <p>
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book
-      </p>
-      {/* learn how to add image */}
-      {/* <img src={} alt="" /> */}
+      <img src={props.images} alt="" />
       <h3> {props.service}</h3>
       <p>{props.description}</p>
     </div>
