e62e9a1bf3beaeda37a7781d90c2fff5642f7776
Gidey <Gidey@Gidey.local>
2020-12-02 20:48:50 -0500

    add animation

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Categories from "./Categories";
 import Services from "./Services";
 import home_banner from "../images/home-banner.png";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function Home() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div className="nave-pages">
       <header className="head">
@@ -28,14 +33,14 @@ function Home() {
         <Categories num="34" description="Happy Clients" />
         <Categories num="34" description="Happy Clients" />
       </div>
-      <div>
+      <div data-aos="fade-up">
         <h1>Service We Provide</h1>
         <p>
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book
         </p>
 
-        <div className="service-items">
+        <div data-aos="fade-up" className="service-items">
           <Services
             service="Website Development"
             description="We design professional looking yet simple websies. OUr designs are search engle and user friendly"
